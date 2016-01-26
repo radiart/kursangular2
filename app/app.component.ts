3484f339e1007b2053258d24d7b6b2daf861ce28
Rafal Radomski <rafal.radomski@hmhco.com>
2016-01-26 17:23:00 +0000

    forms

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,12 @@
-import {Component} from 'angular2/core';
-import {OnInit} from 'angular2/core';
-import {NgFor} from 'angular2/common';
+import {Component, OnInit} from 'angular2/core';
+import {
+    NgFor,
+    FORM_DIRECTIVES,
+    FormBuilder,
+    Validators,
+    ControlGroup,
+    Control
+} from 'angular2/common';
 import {Article} from './article';
 import {ArticleComponent} from './article.component';
 import {ArticleService} from './article.service';
@@ -9,15 +15,30 @@ import {ArticleService} from './article.service';
     selector: 'articles',
     templateUrl: 'app/app.component.html',
     providers: [ArticleService],
-    directives: [ArticleComponent]
+    directives: [ArticleComponent, FORM_DIRECTIVES]
 })
 
 export class AppComponent implements OnInit {
     articles: Article[];
+    articleForm: ControlGroup;
+
+    // Validation
+    title: Control = new Control();
+    link: Control = new Control();
+    description: Control = new Control();
+    image: Control = new Control();
 
     constructor(
-        private _articleService: ArticleService
-    ){}
+        private _articleService: ArticleService, // Service
+        fb: FormBuilder;
+    ) {
+        this.articleForm = fb.group({
+            'title': this.title,
+            'link': this.link,
+            'description': this.description,
+            'image': this.image,
+        });
+    }
 
     getArticles(){
         this._articleService.getArticles()
