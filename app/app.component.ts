86ca98fa5b0e95311b70accff2f1a39db81a2c69
Rafal Radomski <rafal.radomski@hmhco.com>
2016-01-26 12:24:25 +0000

    first directive

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'articles',
+    template: '<div> Articl3asdsad45435s</div>'
+})
+
+export class AppComponent {
+
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+# angular2
+
+Tutorial form angularjs 2 page: http://platforma.strefakursow.pl/kurs/271
+
+## Install
+
+``npm install``
+
+## Run
+
+``npm start``
