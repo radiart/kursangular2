1e05dc030c35b7d13c03bb5508935a555a966e3b
Rafal Radomski <rafal.radomski@hmhco.com>
2016-01-26 16:51:44 +0000

    services

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,30 +1,29 @@
 import {Component} from 'angular2/core';
+import {OnInit} from 'angular2/core';
 import {NgFor} from 'angular2/common';
 import {Article} from './article';
 import {ArticleComponent} from './article.component';
-
+import {ArticleService} from './article.service';
 
 @Component({
     selector: 'articles',
     templateUrl: 'app/app.component.html',
+    providers: [ArticleService],
     directives: [ArticleComponent]
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
     articles: Article[];
-    articles = ARTICLES;
 
+    constructor(
+        private _articleService: ArticleService
+    ){}
+
+    getArticles(){
+        this.articles = this._articleService.getArticles();
+    }
+
+    ngOnInit(){
+        this.getArticles();
+    }
 }
-
-var ARTICLES : Article[] = [
-    new Article(
-        'Title',
-        'Desc',
-        'http://link.com'
-    ),
-    new Article(
-        'Title 2',
-        'Desc 2',
-        'http://link 2.com'
-    )
-]
---
+++ app/article.data.ts
@@ -0,0 +1,14 @@
+import {Article} from './article';
+
+export var ARTICLES : Article[] = [
+    new Article(
+        'Title',
+        'Desc',
+        'http://link.com'
+    ),
+    new Article(
+        'Title 2',
+        'Desc 2',
+        'http://link 2.com'
+    )
+]
