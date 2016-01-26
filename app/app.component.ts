80420f2246e9fa1b7c7693490b247f219a014c80
Rafal Radomski <rafal.radomski@hmhco.com>
2016-01-26 12:34:39 +0000

    ngFor

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,27 @@
 import {Component} from 'angular2/core';
+import {NgFor} from 'angular2/common';
 
 @Component({
     selector: 'articles',
-    template: '<div> Articl3asdsad45435s</div>'
+    template: `
+        <ul>
+            <li *ngFor = " #day of days ">{{ day }}</li>
+        </ul>
+    `
 })
 
 export class AppComponent {
+    days: string[];
 
+    constructor() {
+        this.days = [
+            'Sunday' ,
+            'Monday',
+            'Tuesday',
+            'Wednesday',
+            'Thursday',
+            'Friday',
+            'Saturday'
+        ];
+    }
 }
